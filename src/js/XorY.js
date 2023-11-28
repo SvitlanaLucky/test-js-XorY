02bde53d7337b480bf6ca5347e9a3e4e08bfbdbc
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-11-28 19:06:49 +0200

    add new commit

--- src/js/XorY.js
+++ src/js/XorY.js
@@ -1,5 +1,10 @@
 const container =
   document.querySelector('.js-content');
+  const nameWinner = document.querySelector('.js-winner')
+
+let player = 'X';
+let historyX = [];
+let historyO = [];
 
 const wins = [
   [1, 2, 3],
@@ -12,14 +17,14 @@ const wins = [
   [3, 5, 7],
 ];
 
-let player = 'X';
-let markup = '';
-
-for (let i = 1; i < 10; i += 1) {
-  markup += `<div class="item js-item" data-id="${i}"></div>`;
+function createMarkup() {
+  let markup = '';
+  for (let i = 1; i < 10; i += 1) {
+    markup += `<div class="item js-item" data-id="${i}"></div>`;
+  }
+  container.innerHTML = markup;
 }
-
-container.innerHTML = markup;
+createMarkup();
 
 container.addEventListener(
   'click',
@@ -28,15 +33,48 @@ container.addEventListener(
 
 function onClick(event) {
   const { target } = event;
-  if (
-    !target.classList.contains(
-      'js-item'
-    ) ||
-    target.textContent
-  ) {
+  if (!target.classList.contains('js-item') || target.textContent) {
     return;
+  }
+ 
+  const id = Number(target.dataset.id);
+  let result = false;
+  const isEndGame = historyO.length + historyX.length === 9;
+
+  if (player === 'X') {
+    historyX.push(id);
+    result = isWinner(historyX);
+  } else {
+    historyO.push(id);
+    result = isWinner(historyO);
   }
 
   target.textContent = player;
+
+  if (result) {
+    nameWinner.textContent = `winner ${player} 😎`;
+    setTimeout(() => {
+      resetGame();
+      }, 1500);
+    
+    return;
+  } else if (isEndGame) {
+    console.log(`Try again 😮`);
+    resetGame();
+    return;
+  }
   player = player === 'X' ? '0' : 'X';
+}
+
+function isWinner(arr) {
+  return wins.some((item) =>
+    item.every((id) => arr.includes(id))
+  );
+}
+
+function resetGame() {
+  createMarkup();
+  historyX = [];
+  historyO = [];
+  player = 'X';
 }
